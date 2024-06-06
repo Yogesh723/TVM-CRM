15e6615ec2baf4cefafab09c03be5926c19b033c
Yogesh723 <yogesh61956@gmail.com>
2024-06-06 12:14:06 +0530

    login commit

--- src/app/common/nav-bar/nav-bar.component.ts
+++ src/app/common/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { NavigationServiceService } from '../services/navigation-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,12 +11,25 @@ export class NavBarComponent {
   @Input() topNavMenu: any = [];
 
   constructor(
-    private navService: NavigationServiceService
+    private navService: NavigationServiceService,
+    private router: Router
   ) {
 
   }
 
   goBack(event: any) {
     this.navService.back();
+  }
+
+  user = {
+    name: 'John Doe',  // Replace this with the actual user name
+  };
+
+  
+
+  logout() {
+    // Clear user session or token
+    // Navigate to the login page
+    this.router.navigate(['/login']);
   }
 }
